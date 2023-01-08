43c618061a2c1d2ebffe000e2b51ff4fb9bed6ff
Anshul Hajare <anshulhajare2024@u.northwestern.edu>
2023-01-08 17:40:54 -0600

    Task D

--- generate.py
+++ generate.py
@@ -1,5 +1,12 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("box.sdf")
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
+pyrosim.Start_SDF("boxes.sdf")
+
+for x in range(0, 5):
+    for y in range(0, 5):
+        z = 0.5
+        for dir in range(10):
+            pyrosim.Send_Cube(name="Box", pos=[x, y, z], size=[(0.9**dir), (0.9**dir), (0.9**dir)])
+            z += 1
+
 pyrosim.End()
