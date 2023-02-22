1e9da50cdbf9711bc5a57382f263bfd15da35dba
Anshul Hajare <anshulhajare2024@u.northwestern.edu>
2023-02-22 00:11:01 -0600

    Update simulation

--- simulation.py
+++ simulation.py
@@ -17,26 +17,25 @@ from sensor import SENSOR
 class SIMULATION:
     def __init__(self, directOrGUI="GUI", solutionID=0):
         directOrGUI = "GUI"
-        if directOrGUI == "DIRECT":
-            self.physicsClient = p.connect(p.DIRECT)
-        else:
-            self.physicsClient = p.connect(p.GUI)
-            p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
-        p.setAdditionalSearchPath(pybullet_data.getDataPath())
-        p.setGravity(0, 0, -100.8)
-        #self.world = WORLD()
+        # if directOrGUI == "DIRECT":
+        #     self.physicsClient = p.connect(p.DIRECT)
+        # else:
+        self.physicsClient = p.connect(p.GUI)
+        p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
+        # p.setAdditionalSearchPath(pybullet_data.getDataPath())
+        p.setGravity(0, 0, -9.8)
         self.robot = ROBOT(solutionID)
 
     def Get_Fitness(self, solutionID):
         self.robot.Get_Fitness(solutionID)
 
     def Run(self):
-        for i in range(500): # "The FOR LOOP"
+        for i in range(500):
             p.stepSimulation()
             self.robot.Sense(i)
             self.robot.Think()
             self.robot.Act(i, self.robot.robotId)
-            time.sleep(1/500)
+            time.sleep(1/100)
 
     def __del__(self):
         p.disconnect()
